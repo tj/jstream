0a00cb0e1e4f4310326bdc9060d544f9928705ae
TJ Holowaychuk <tj@vision-media.ca>
2013-03-14 14:53:19 -0700

    add calls assertion to string-notation test

--- test/string-notation.js
+++ test/string-notation.js
@@ -3,11 +3,13 @@ var fs = require ('fs')
   , json = require('..')
   , assert = require('assert');
 
-var parser = json.parse([true, 'name', 'first']);
+var parser = json.parse('*.name.first');
 
 var names = ['Tobi', 'Loki', 'Jane'];
+var calls = 0;
 
 parser.on('data', function(obj){
+  ++calls;
   assert(~names.indexOf(obj));
 });
 
@@ -16,3 +18,7 @@ parser.write('  { "name": { "first": "Tobi" } },');
 parser.write('  { "name": { "first": "Loki" } },');
 parser.write('  { "name": { "first": "Jane" } }');
 parser.write(']');
+
+process.on('exit', function(){
+  assert(3 == calls);
+});
